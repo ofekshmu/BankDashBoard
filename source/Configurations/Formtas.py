421bdfac999b553fab3fa87edcaad7fd1c6cf38a
Ofek Shmuel <ofek.shmuel1@gmail.com>
2023-06-16 18:33:33 +0300

    Trying a new generic method of adding new formats

---
+++ source/Configurations/Formtas.py
@@ -0,0 +1,36 @@
+########################################################################
+#                             Format File
+#
+#  Mind that on WINDOWS OS, all slashes are as follows: /
+#
+########################################################################
+from enum import Enum
+
+class Method(Enum):
+    FILE_NAME = 1
+    CELL = 2
+    HEADERS = 3
+    NONE = 4
+
+
+class Sortion(Enum):
+    BY_NAME_SERIAL = 1
+    BY_NAME_DATE = 2
+
+
+class Context(Enum):
+    Card = 1
+    Bank = 2
+
+
+class Formats:
+
+    FORMATS = [{"Format Name": "Leumi-Bank",
+                "Context": Context.Card,
+                "Identification method": None,
+                "Identification data": None,
+                "Sortion method": None,
+                "Sortion key": None,
+                "Headers": [],
+                "Header row index": None}
+                ]

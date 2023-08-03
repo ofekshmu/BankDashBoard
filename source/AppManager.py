9ebe7c7a78457adb0cf6c06b8c60d6f850171c55
Ofek Shmuel <ofek.shmuel1@gmail.com>
2023-08-03 07:51:59 +0300

    added gas stats

--- source/AppManager.py
+++ source/AppManager.py
@@ -154,7 +154,6 @@ class AppManager:
 
         spendings, description = DataBase().get_monthly_spendings(year=t.year, month=t.month)
         spendings_df = SimpleMath.process_prices(spendings, description)
-        print(spendings_df.to_markdown(), spendings_df.columns)
         earnings, description = DataBase().get_monthly_earnings(year=t.year, month=t.month)
         earnings_df = SimpleMath.process_prices(earnings, description)
         end_monthly_balance = -1

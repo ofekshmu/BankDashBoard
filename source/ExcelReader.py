39139c3cf09dcafe478e8d07f482de94285ca847
Ofek Shmuel <ofek.shmuel1@gmail.com>
2023-10-20 15:00:22 +0300

    Excel Reader working

--- source/ExcelReader.py
+++ source/ExcelReader.py
@@ -1,5 +1,6 @@
 import xlwings as xw
 
+
 class ExcelManager:
     _instance = None
 
@@ -17,25 +18,15 @@ class ExcelManager:
                 workbook = self.app.books.open(file_path)
                 sheet = workbook.sheets[0]  # Read the first sheet in the Excel file
                 self.active_sheet = sheet
-            return self.active_sheet
+            return True
         except Exception as e:
             print(f"Error opening file or sheet: {str(e)}")
-            return None
+            return False
 
-    def read_values(self, start_cell, end_cell):
-        if self.active_sheet is not None:
-            try:
-                data_range = self.active_sheet.range(start_cell, end_cell)
-                return data_range.value
-            except Exception as e:
-                print(f"Error reading data from sheet '{self.active_sheet}': {str(e)}")
-            finally:
-                self.close_and_kill_excel()
-
-        return None
 
     def set_active_sheet(self, file_path):
         if self.active_sheet is not None:
+            self.active_sheet.book.close()
             self.active_sheet = None  # Close the previous active sheet
 
         if not self.open_sheet(file_path):
@@ -47,14 +38,29 @@ class ExcelManager:
             self.app.quit()
             self.active_sheet = None
 
+    def read_sheet(self, row_idx: int, row_count: int, col_idx: int, col_count: int) -> list:
+        """
+        The function read the a table like structure out of an excel file names @file_name
+        The indexes are inclusive, meaning that data will be read from row_idx until row_idx + row_count
+        including the values of the border indexes.
+        """
+        if not self.open_sheet(self.active_sheet):
+            print(f"Error setting active sheet to '{file_path}': Sheet not found")
+        if self.active_sheet is not None:
+            return self.active_sheet[row_idx - 1: row_idx - 1 + row_count, col_idx: col_idx + col_count].value
+
 
 if __name__ == "__main__":
     excel_manager = ExcelManager()
 
     file_path = r"C:\Users\ofeks\OneDrive\Work\Projects\Personal\BankProject\Inputs01\isra"
     file_1 = "Export_11_2023.xls"
+    file_2 = "Export_10_2023.xls"
     excel_manager.set_active_sheet(f"{file_path}\{file_1}")
-    data = excel_manager.read_values("A1", "B3")
-    if data:
-        for row in data:
-            print(row)
+
+    print(excel_manager.read_sheet(1, 15, 1, 15))
+    print(excel_manager.read_sheet(1, 3, 1, 3))
+    excel_manager.set_active_sheet(f"{file_path}\{file_2}")
+    print(excel_manager.read_sheet(1, 5, 1, 5))
+
+    excel_manager.close_and_kill_excel()

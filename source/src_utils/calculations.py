a9baede8ec33f744089753be52d23ae1c3036668
Ofek Shmuel <ofek.shmuel1@gmail.com>
2025-10-17 19:51:57 +0300

    Refactored the get_monthly_shifted to fit general query of transactions and more efficient

--- source/src_utils/calculations.py
+++ source/src_utils/calculations.py
@@ -96,70 +96,43 @@ class SimpleMath:
         all prices queried are being proccesed.
         """
 
-        def generate_date_range(n) -> list[str]:
-            """
-            The function a range of dates, from the last month, n months back.
-            the dates will be in '%Y-%m' format.
-            for the current date 29/6/24 and n = 6, the following will be returned:
-            ['2023-12', '2024-01', '2024-02', '2024-03', '2024-04', '2024-05']
-            """
-            delta = 0 if GENERAL_PLOT.SHOW_CURRENT_MONTH else 1
-            # Get the current date
-            current_date = datetime.now() - pd.DateOffset(months=delta)
-            # Calculate the start date (n months back from the current month)
-            start_date = (current_date.replace(day=1) - pd.DateOffset(months=n-1)).replace(day=1)
-            # Generate a range of dates from the start date to the current month
-            date_range = pd.date_range(start=start_date, end=current_date, freq='MS')
-            # Convert the date range to the format '%Y-%m'
-            date_range_str = date_range.strftime('%Y-%m').tolist()
-            return date_range_str
+        from Constants import INVESTMENT_CATEGORY
 
-        # Data is queried and proccessed
-        earnings_df = SimpleMath.process_prices(DataBase().get_earnings())
-        spendings_df = SimpleMath.process_prices(DataBase().get_spendings())
+        calculated_date = datetime.now()
+        initial_delta = 0 if GENERAL_PLOT.SHOW_CURRENT_MONTH else 1
+        spendings_lst = []
+        spendings_net_lst = []
+        earnings_lst = []
 
-        # filter the data according to the given arguments
-        if category is not None:
-            # Handle both single category (string) and multiple categories (list)
-            if isinstance(category, str):
-                category = [category]
-            earnings_df = earnings_df[earnings_df['Category'].isin(category)]
-            spendings_df = spendings_df[spendings_df['Category'].isin(category)]
-        if business is not None:
-            # Handle both single business (string) and multiple businesses (list)
-            if isinstance(business, str):
-                business = [business]
-            earnings_df = earnings_df[earnings_df['Name'].isin(business)]
-            spendings_df = spendings_df[spendings_df['Name'].isin(business)]
+        for _ in range(initial_delta, shift):
+            calculated_date = calculated_date - pd.DateOffset(months=1)
+            df_i = SimpleMath.process_prices(
+                        DataBase().query_monthly_transactions(date=calculated_date,
+                                                              tables=['BankTransactions','CardTransactions']),
+                        date=calculated_date)
+            
+            if category is not None:
+                if isinstance(category, str):
+                    category = [category]
+                df_i = df_i[df_i['Category'].isin(category)]
 
-        # Date format is converted to month resolution in order to enable proper 'Group-by'
-        earnings_df['Date/Executed_Date'] = pd.to_datetime(earnings_df['Date/Executed_Date'], format="%Y-%m-%d %H:%M:%S").apply(lambda x: x.strftime('%Y-%m'))
-        spendings_df['Date/Executed_Date'] = pd.to_datetime(spendings_df['Date/Executed_Date'], format="%Y-%m-%d %H:%M:%S").apply(lambda x: x.strftime('%Y-%m'))
+            if business is not None:
+                if isinstance(business, str):
+                    business = [business]
+                df_i = df_i[df_i['Name'].isin(business)]
 
-        # date range is generated
-        full_date_df = pd.DataFrame(generate_date_range(shift), columns=['Date/Executed_Date'])
-        
-        # conversion of all relevant data columns into datetime format
-        full_date_df['Date/Executed_Date'] = pd.to_datetime(full_date_df['Date/Executed_Date'], format='%Y-%m')
-        earnings_df['Date/Executed_Date'] = pd.to_datetime(earnings_df['Date/Executed_Date'], format='%Y-%m')
-        spendings_df['Date/Executed_Date'] = pd.to_datetime(spendings_df['Date/Executed_Date'], format='%Y-%m')
+            if df_i.empty:
+                utils.log("test", 'system')
 
-        # Merge the full date range DataFrame with the original DataFrame
-        earnings_df = pd.merge(full_date_df, earnings_df, on='Date/Executed_Date', how='left')
-        spendings_df = pd.merge(full_date_df, spendings_df, on='Date/Executed_Date', how='left')
-        
-        # calculation of net income
-        # This section is not relevant when calculating the monthly shifted data per category but for all transactions
-        spendings_net_df = spendings_df[spendings_df['Category'] != 'השקעה/חיסכון']
-        
-        earnings_df = earnings_df.groupby('Date/Executed_Date').sum()
-        spendings_df = spendings_df.groupby('Date/Executed_Date').sum()
-        spendings_net_df = spendings_net_df.groupby('Date/Executed_Date').sum()
 
+            spendings_lst.append(df_i['Final_Value'][(df_i['Final_Value'] < 0)].sum())
+            spendings_net_lst.append(df_i['Final_Value'][(df_i['Final_Value'] < 0) & (df_i['Category'] != INVESTMENT_CATEGORY)].sum())
+            earnings_lst.append(df_i['Final_Value'][(df_i['Final_Value'] > 0)].sum())
+            
         # data is returned backwards to fit the plot_general function.
-        return list(spendings_df['Final_Value'])[::-1], \
-                list(spendings_net_df['Final_Value'])[::-1], \
-                list(earnings_df['Final_Value'])[::-1]
+        return spendings_lst[::-1], \
+                spendings_net_lst[::-1], \
+                earnings_lst[::-1]
 
     @staticmethod
     def general_info(data):
@@ -277,7 +250,7 @@ class SimpleMath:
                 @return (Trans_Type(Enum) = "flowing", bool) - true if the executed month matches the given date month else, false.
             """
             return row['Executed_Date'],\
-                    (-row['transaction_value']),\
+                    (-row['Transaction_Value']),\
                     Trans_Type.flowing,\
                      date.month == pd.to_datetime(row['Executed_Date']).month
     
@@ -329,6 +302,7 @@ class SimpleMath:
                 return pd.Series([row['Executed_Date'], -row['Transaction_Value'], Trans_Type.default, True])
 
         if df.empty:
+            utils.log("df is empty", "debug")
             return df
         
         df[['Executed_Date', 'Final_Value', 'Transaction_Type', 'Relevance']]  = df.apply(classify_and_handle, axis=1)

c41e844e5338c3439bfb5a512064edbb1e209987
Ofek Shmuel <ofek.shmuel1@gmail.com>
2026-05-22 21:51:04 +0300

    feat: add activity log page and routes

---
+++ source/routes/activity_routes.py
@@ -0,0 +1,26 @@
+from flask import Blueprint, render_template, session
+from auth import AuthManager
+from database import DataBase
+from routes.auth_routes import require_login
+
+activity_bp = Blueprint('activity', __name__)
+db = DataBase()
+
+@activity_bp.route('/activity')
+@require_login
+def activity_log():
+    """Display login/logout activity log"""
+    activities = db.get_login_activity()
+
+    # Format data for display
+    formatted_activities = []
+    for activity in activities:
+        formatted_activities.append({
+            'id': activity['id'],
+            'login_time': activity['login_time'].strftime('%Y-%m-%d %H:%M:%S') if activity['login_time'] else 'N/A',
+            'logout_time': activity['logout_time'].strftime('%Y-%m-%d %H:%M:%S') if activity['logout_time'] else 'Still logged in',
+            'device_info': activity['device_info'],
+            'ip_address': activity['ip_address']
+        })
+
+    return render_template('activity_log.html', activities=formatted_activities)
